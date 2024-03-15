650ac394a84651ebd5ed2a430191f36b0820328f
ARC <>
2024-03-15 17:26:32 -0600

    Actualización de la conexión a la BD mediante archivo de configuración

---
+++ src/MODELO/Conexion.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MODELO;
+
+import java.net.InetAddress;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+import java.util.Properties;
+import controlinterno.ConfigDB;
+import java.sql.SQLException;
+/**
+ *
+ * @author Marcos Luis Goxcon Portillo
+ */
+public class Conexion {   
+    public Conexion(){
+
+    }
+    public Connection conectar(){
+        Connection con = null;
+        try {
+            ConfigDB config = new ConfigDB();
+            
+            if (!config.OK){
+                throw new SQLException("Falta paramétros. Revise el archivo config.properties");
+            }
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            con = DriverManager.getConnection(config.URL, config.USER, config.PASSWD);
+        } catch (Exception e) {
+            //e.printStackTrace();
+            JOptionPane.showMessageDialog(null,"Error "+  e + "\n\n El sistema se cerrará");
+            System.exit(0);
+            //JOptionPane.showMessageDialog(null, "Error de comunicación con el servidor :( ","Error al conectar",JOptionPane.ERROR_MESSAGE);
+            //System.out.print(e);
+        }
+        return con;
+    }
+    
+}
